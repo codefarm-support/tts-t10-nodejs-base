5cf41bb18dc29200ff55171c42c2f81939fde4f6
CodeFarm <support@codefarm.edu.vn>
2025-10-01 16:48:14 +0700

    build base auth for intern

---
+++ controllers/auth.controller.js
@@ -0,0 +1,15 @@
+export const registerController = async (req, res) => {
+  try {
+    console.log("Dang ky thanh cong");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const loginController = async (req, res) => {
+  try {
+    console.log("Dang nhap thanh cong");
+  } catch (error) {
+    console.log(error);
+  }
+};
